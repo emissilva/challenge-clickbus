e8af66e7a3fd45b5cf8e948270eb90a0e0daa141
Emerson <emerson.esilva@icloud.com>
2025-08-29 00:32:12 -0300

    atualização do problema1.py

--- scripts/problema1.py
+++ scripts/problema1.py
@@ -2,11 +2,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-# Carregar os dados
+# 1. Carregar os dados
 df = pd.read_csv('data/df_tratado.csv')
 
 
-# Pré-processamento e Cálculo das Métricas RFM
+# 2. Pré-processamento e Cálculo das Métricas RFM
 df['purchase_datetime'] = pd.to_datetime(df['date_purchase'] + ' ' + df['time_purchase'])
 today = df['purchase_datetime'].max() + pd.Timedelta(days=1)
 rfm_df = df.groupby('fk_contact').agg(
@@ -15,17 +15,17 @@ rfm_df = df.groupby('fk_contact').agg(
     monetary=('gmv_success', 'sum')
 ).reset_index()
 
-# Escalar os dados para o K-Means
+# 3. Escalar os dados para o K-Means
 features = rfm_df[['recency', 'frequency', 'monetary']]
 scaler = StandardScaler()
 scaled_features = scaler.fit_transform(features)
 
-# Aplicar a clusterização K-Means (com 3 clusters para o MVP)
+# 4. Aplicar a clusterização K-Means (com 3 clusters para o MVP)
 kmeans = KMeans(n_clusters=3, random_state=42, n_init='auto')
 kmeans.fit(scaled_features)
 rfm_df['cluster'] = kmeans.labels_
 
-# Salvar o resultado
+# 5. Salvar o resultado
 rfm_df.to_csv('data/resultados/resultado_p1.csv', index=False)
 
 print("Clusterização concluída. O arquivo 'resultado_p1.csv' foi salvo com sucesso.")
